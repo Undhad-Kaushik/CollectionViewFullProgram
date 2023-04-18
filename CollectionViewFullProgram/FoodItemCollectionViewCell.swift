8c62dcc62ecd442d1f1b202048a3231953347dfd
Undhad Kaushik <kpkushpatel8@gmail.com>
2023-04-18 08:36:27 +0530

    Initial Commit

---
+++ CollectionViewFullProgram/FoodItemCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  FoodItemCollectionViewCell.swift
+//  CollectionViewFullProgram
+//
+//  Created by undhad kaushik on 04/01/23.
+//
+
+
+
+import UIKit
+
+class FoodItemCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderWidth = 1
+        layer.cornerRadius = 10
+        layer.masksToBounds = true
+    }
+
+}
---
+++ CollectionViewFullProgram/MobileItemCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  MobileItemCollectionViewCell.swift
+//  CollectionViewFullProgram
+//
+//  Created by undhad kaushik on 04/01/23.
+//
+
+
+
+import UIKit
+
+class MobileItemCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
